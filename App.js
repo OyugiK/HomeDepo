30c6dd53cce6599e830a3c175d925371fb190499
OyugiK <Oyugi.K.Owino@gmail.com>
2018-11-20 13:27:59 +0300

    bug fix for removed css contanier

--- App.js
+++ App.js
@@ -4,8 +4,11 @@ import { StyleSheet, Text, View } from 'react-native';
 export default class App extends React.Component {
   render() {
     return (
-      <View style={styles.container}>
-        <Text>Welcome to our awesome crossplatform app</Text>
+      <View>
+        <Text>Open up App.js to start working on your app!</Text>
+        <Text>Happy to be here!</Text>
+        <Text>Changes made to the App.js file will be automcatically</Text>
+        <Text>Shake the phone to open developer menu</Text>
       </View>
     );
   }
