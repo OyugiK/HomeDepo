ff0d1d707420c78cf32f453a606d101b849232a1
OyugiK <Oyugi.K.Owino@gmail.com>
2018-11-20 13:21:19 +0300

    extra comment for loading on the emulator

--- App.js
+++ App.js
@@ -7,6 +7,7 @@ export default class App extends React.Component {
       <View style={styles.container}>
         <Text>Open up App.js to start working on your app!</Text>
           <Text>Happy to be here!</Text>
+          <Text>Changes made to the App.js file will be automcatically loaded on the emulator</Text>
       </View>
     );
   }
