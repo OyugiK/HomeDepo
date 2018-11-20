84566edb9d5a92dee84938e0eafe050af27cdee2
OyugiK <Oyugi.K.Owino@gmail.com>
2018-11-20 13:22:38 +0300

    simple syntax changes

--- App.js
+++ App.js
@@ -6,8 +6,8 @@ export default class App extends React.Component {
     return (
       <View style={styles.container}>
         <Text>Open up App.js to start working on your app!</Text>
-          <Text>Happy to be here!</Text>
-          <Text>Changes made to the App.js file will be automcatically loaded on the emulator</Text>
+        <Text>Happy to be here!</Text>
+        <Text>Changes made to the App.js file will be automcatically loaded on the emulator</Text>
       </View>
     );
   }
