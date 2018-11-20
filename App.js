7da603d0645e29bf024f9de8ef916f9c13a2a2ce
OyugiK <Oyugi.K.Owino@gmail.com>
2018-11-20 13:38:47 +0300

    state examples

--- App.js
+++ App.js
@@ -3,12 +3,7 @@ import { StyleSheet, Text, View } from 'react-native';
 
 export default class App extends React.Component {
    state = {
-      myState: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, used do eiusmod
-      tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis
-      nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
-      Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
-      fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-      culpa qui officia deserunt mollit anim id est laborum.'
+        myState: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, used do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quisnostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eufugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt inculpa qui officia deserunt mollit anim id est laborum.'
    }
    render() {
       return (
@@ -17,3 +12,4 @@ export default class App extends React.Component {
       </View>
       );
    }
+}
