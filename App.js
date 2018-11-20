b84fdc40f0ac4122e529f354e7023beeca44009b
OyugiK <Oyugi.K.Owino@gmail.com>
2018-11-20 13:26:26 +0300

    removed css bits

--- App.js
+++ App.js
@@ -5,20 +5,8 @@ export default class App extends React.Component {
   render() {
     return (
       <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <Text>Happy to be here!</Text>
-        <Text>Changes made to the App.js file will be automcatically</Text>
-        <Text>Shake the phone to open developer menus</Text>
+        <Text>Welcome to our awesome crossplatform app</Text>
       </View>
     );
   }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
